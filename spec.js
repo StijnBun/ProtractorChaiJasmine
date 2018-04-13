a40c216320daa9a77c39470dab99aa50728fddf3
Stijn Bunschoten <stijnbunschoten@BA92-2T81K5GTDY.local>
2018-04-13 13:55:36 +0200

    fix

--- spec.js
+++ spec.js
@@ -2,18 +2,13 @@ var chai = require("chai");
 var chaiAsPromised = require("chai-as-promised");
 browser.waitForAngularEnabled(false);
 chai.use(chaiAsPromised);
-
-// Then either:
 var expect = chai.expect;
 
-// spec.js
-describe('Protractor Demo App', function() {
-  it('should add one and two', function(done) {
+describe('Protractor Chai Jasmin Demonstratie', function() {
+  it('Find the road to ENLIGHTENMENT', function(done) {
     browser.get('http://www.csszengarden.com/');
     var gottenelement = element(by.xpath('//*[@id="zen-preamble"]/h3'))
-    expect(gottenelement.getText()).to.eventually.equal('The Road to Enlightenment').and.notify(done);
-    //     toEqual('3'); // This is wrong!
-    //     done();
+    expect(gottenelement.getText()).to.eventually.equal('THE ROAD TO ENLIGHTENMENT').and.notify(done);
 
   });
 });
